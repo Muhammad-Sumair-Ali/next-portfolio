6e10e6b00bdcd82e84df6cd6653ce0fce3a2e452
Muhammad-Sumair-Ali <sumairjattoi@gmail.com>
2025-04-04 01:56:05 +0500

    ui isue fix

--- src/components/reuseable/Hero.tsx
+++ src/components/reuseable/Hero.tsx
@@ -72,7 +72,10 @@ export default function Hero() {
                     transition={{ type: "tween", duration: 0.3 }}
                     className="inline-flex items-center font-bold justify-center whitespace-nowrap"
                   >
-                    <span className={textItem.className}>
+                    <span
+                      className={textItem.className}
+                      style={{ WebkitBackgroundClip: "text" }}
+                    >
                       {textItem.key.charAt(0).toUpperCase() +
                         textItem.key.slice(1)}
                     </span>
