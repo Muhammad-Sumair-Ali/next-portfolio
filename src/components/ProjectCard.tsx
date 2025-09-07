c86169c14378361aa0d2063319bb578988db4f98
Muhammad-Sumair-Ali <sumairjattoi@gmail.com>
2025-09-07 14:42:29 -0700

    fix card links

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -6,12 +6,6 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
 import { ExternalLink, Github, Info } from "lucide-react";
 
 interface Project {
@@ -34,24 +28,23 @@ export function ProjectCard({ project }: { project: Project }) {
     demoLink,
     githubLink,
     tags,
-    _id
+    _id,
   } = project;
-  const [showDetails, setShowDetails] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const checkMobile = () => {
       setIsMobile(window.innerWidth < 768);
     };
-    
+
     // Check initially
     checkMobile();
-    
+
     // Add event listener
-    window.addEventListener('resize', checkMobile);
-    
+    window.addEventListener("resize", checkMobile);
+
     // Cleanup
-    return () => window.removeEventListener('resize', checkMobile);
+    return () => window.removeEventListener("resize", checkMobile);
   }, []);
 
   return (
@@ -66,22 +59,26 @@ export function ProjectCard({ project }: { project: Project }) {
 
         {/* Card Image */}
         <Link href={`/project/${_id}`}>
-        <div className="relative  aspect-video cursor-pointer overflow-hidden">
-          <Image
-            priority={false}
-            src={imageUrl}
-            alt={title}
-            fill
-            className="object-cover transition-transform duration-300 group-hover:scale-110"
-          />
-          <div className="absolute inset-0 bg-white/10 dark:bg-black/50 opacity-0 transition-opacity group-hover:opacity-100" />
-        </div>
+          <div className="relative  aspect-video cursor-pointer overflow-hidden">
+            <Image
+              priority={false}
+              src={imageUrl}
+              alt={title}
+              fill
+              className="object-cover transition-transform duration-300 group-hover:scale-110"
+            />
+            <div className="absolute inset-0 bg-white/10 dark:bg-black/50 opacity-0 transition-opacity group-hover:opacity-100" />
+          </div>
         </Link>
 
         <CardContent className="relative z-10 px-4 py-4 md:p-6 flex-grow overflow-hidden">
-          <motion.h3 className="mb-2 text-2xl font-bold text-black dark:text-white line-clamp-1">
+          <Link href={`/project/${_id}`}>
+          <motion.h3 className="mb-2 text-2xl font-bold text-black dark:text-white dark:hover:text-blue-500 hover:text-blue-500 hover:underline cursor-pointer line-clamp-1">
             {title}
-          </motion.h3>          {/* Tags */}          <motion.div className="flex flex-wrap gap-2 mb-3 relative">
+          </motion.h3>{" "}
+          </Link>
+          {/* Tags */}{" "}
+          <motion.div className="flex flex-wrap gap-2 mb-3 relative">
             {/* Show all tags on mobile, limit on larger screens */}
             {(isMobile ? tags : tags.slice(0, 8)).map((tag, index) => (
               <span
@@ -92,15 +89,14 @@ export function ProjectCard({ project }: { project: Project }) {
               </span>
             ))}
             {!isMobile && tags.length > 8 && (
+                        <Link href={`/project/${_id}`}>
               <span
-                className="rounded-full bg-gray-300 dark:bg-zinc-800 px-3 py-1 text-xs font-medium text-black dark:text-zinc-200 transition-colors hover:bg-gray-400 dark:hover:bg-zinc-700 cursor-pointer"
-                onClick={() => setShowDetails(true)}
-              >
+                className="rounded-full bg-gray-300 dark:hover:text-blue-500 hover:text-blue-500 hover:underline dark:bg-zinc-800 px-3 py-1 text-xs font-medium text-black dark:text-zinc-200 transition-colors hover:bg-gray-400 dark:hover:bg-zinc-700 cursor-pointer" >
                 +{tags.length - 8} more...
               </span>
+                  </Link>
             )}
           </motion.div>
-
           <motion.p className="text-sm text-gray-700 dark:text-zinc-300 line-clamp-2">
             {description}
           </motion.p>
@@ -153,97 +149,19 @@ export function ProjectCard({ project }: { project: Project }) {
               </Button>
             )}
           </div>
-          <Button
-            variant="ghost"
-            size="sm"
-            className="text-gray-600 dark:text-zinc-400 hover:bg-gray-300 dark:hover:bg-zinc-800 bg-gray-200 dark:bg-zinc-800 rounded-lg hover:text-black dark:hover:text-zinc-200"
-            onClick={() => setShowDetails(true)}
-          >
-            <Info className="mr-2 h-4 w-4" />
-            More
-          </Button>
+          <Link href={`/project/${_id}`}>
+            <Button
+              variant="ghost"
+              size="sm"
+              className="text-gray-600 dark:hover:text-blue-500 hover:text-blue-500 hover:underline dark:text-zinc-400 hover:bg-gray-300 dark:hover:bg-zinc-800 bg-gray-200 dark:bg-zinc-800 rounded-lg "
+            >
+              <Info className="mr-2 h-4 w-4" />
+              More <span className="hidden sm:block">Details</span>
+            </Button>
+          </Link>
         </CardFooter>
       </Card>
 
-      {/* Dialog for Details */}
-      <Dialog open={showDetails} onOpenChange={setShowDetails}>
-        <DialogContent className="max-w-2xl border-0 bg-white dark:bg-zinc-900 text-black dark:text-white">
-          <DialogHeader>
-            <DialogTitle className="text-3xl font-bold">{title}</DialogTitle>
-          </DialogHeader>
-
-          <div className="relative aspect-video w-full h-48 overflow-hidden rounded-lg">
-            <Image
-              priority={false}
-              src={imageUrl}
-              alt={title}
-              fill
-              className="object-contain bg-gray-200 dark:bg-zinc-800"
-            />
-          </div>
-
-          <div className="space-y-4">
-            <div className="flex flex-wrap gap-2">
-              {tags.map((tag, index) => (
-                <span
-                  key={`${tag}-${index}`}
-                  className="rounded-full bg-gray-300 dark:bg-zinc-800 px-3 py-1 text-xs font-medium text-black dark:text-zinc-200 transition-colors hover:bg-gray-400 dark:hover:bg-zinc-700"
-                >
-                  {tag}
-                </span>
-              ))}
-            </div>
-            <p className="text-gray-700 dark:text-zinc-300">
-              {longDescription || description}
-            </p>
-            <div className="flex gap-4">
-              {demoLink ? (
-                <Button
-                  asChild
-                  size="sm"
-                  className="bg-zinc-700 text-zinc-200 hover:bg-zinc-400 hover:text-zinc-900"
-                >
-                  <Link href={demoLink} target="_blank">
-                    <ExternalLink className="mr-2 h-4 w-4" />
-                    Live Demo
-                  </Link>
-                </Button>
-              ) : (
-                <Button
-                  disabled
-                  size="sm"
-                  className="bg-zinc-700 text-zinc-200 opacity-50 cursor-not-allowed"
-                >
-                  <ExternalLink className="mr-2 h-4 w-4" />
-                  Live Demo
-                </Button>
-              )}
-
-              {githubLink ? (
-                <Button
-                  asChild
-                  size="sm"
-                  className="bg-zinc-700 text-zinc-200 hover:bg-zinc-400 hover:text-zinc-900"
-                >
-                  <Link href={githubLink} target="_blank">
-                    <Github className="mr-2 h-4 w-4" />
-                    View Code
-                  </Link>
-                </Button>
-              ) : (
-                <Button
-                  disabled
-                  size="sm"
-                  className="bg-zinc-700 text-zinc-200 opacity-50 cursor-not-allowed"
-                >
-                  <Github className="mr-2 h-4 w-4" />
-                  View Code
-                </Button>
-              )}
-            </div>
-          </div>
-        </DialogContent>
-      </Dialog>
     </motion.div>
   );
 }
